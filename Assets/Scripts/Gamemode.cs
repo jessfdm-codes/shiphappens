acf22b806c419d704b90d479d60ea2226dbd5cfe
Sam Law <SamJamesLaw@gmail.com>
2018-01-27 12:04:47 +0000

    ShipEater instantiated in Gamemode script

--- Assets/Scripts/Gamemode.cs
+++ Assets/Scripts/Gamemode.cs
@@ -30,7 +30,8 @@ public class Gamemode : MonoBehaviour
         speed = 1;
         score = 0;
         isPlaying = true;
-        SpawnShip();
+        SpawnShipEater();
+        SpawnShip(Spawn1);
     }
 
     // Update is called once per frame
@@ -39,10 +40,22 @@ public class Gamemode : MonoBehaviour
 
     }
 
-    //Spawn a single ship
-    void SpawnShip()
+    void SpawnShipEater()
     {
-        GameObject go = (GameObject)Instantiate(shipPrefab, Spawn1.position, transform.rotation);
+        //CapsuleCollider ShipEaterClone = new CapsuleCollider();
+        GameObject ShipEater = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        ShipEater.name = "ShipEater";
+        ShipEater.transform.position = new Vector3(0, 5, -1);
+        ShipEater.GetComponent<CapsuleCollider>().height = 15;
+        ShipEater.GetComponent<CapsuleCollider>().radius = 2.5f;
+        Destroy(ShipEater.GetComponent<MeshFilter>());
+        
+    }
+
+    //Spawn a single ship
+    void SpawnShip(Transform spawn)
+    {
+        GameObject go = (GameObject)Instantiate(shipPrefab, spawn.position, transform.rotation);
         go.GetComponent<ShipAI>().Initialize(difficulty, speed, this.gameObject.GetComponent<Transform>());
     }
 
