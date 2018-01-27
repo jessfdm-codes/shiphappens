4b57f285fdb8b7118966c7a10fd8cf0ac5657f3b
Sam Law <SamJamesLaw@gmail.com>
2018-01-27 11:05:53 +0000

    Finished basic ship AI
    A ship now spawns at Spawn1 and advances towards the platform, destroys when it reaches the platform

--- Assets/Scripts/Gamemode.cs
+++ Assets/Scripts/Gamemode.cs
@@ -2,41 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gamemode : MonoBehaviour {
+public class Gamemode : MonoBehaviour
+{
 
     [SerializeField]
     int score; //score for the player
     bool isPlaying; //whether we're playing or not
     int difficulty; //ramp up as game goes on?
-	[SerializeField]
+    float speed; //change according to ship when we introduce types of ship?
+    [SerializeField]
     GameObject shipPrefab;
-	
-	//Spawn points for ships
-	[SerializeField]
-	Transform Spawn1;
-	[SerializeField]
-	Transform Spawn2;
-	[SerializeField]
-	Transform Spawn3;
-    
-	
+
+    //Spawn points for ships
+    [SerializeField]
+    Transform Spawn1;
+    [SerializeField]
+    Transform Spawn2;
+    [SerializeField]
+    Transform Spawn3;
+
+
 
     // Use this for initialization
-    void Start () {
-		difficulty = 0;
-		score = 0;
-		isPlaying = false;
-		SpawnShip();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	//Spawn a single ship
-	void SpawnShip () {
-		GameObject go = Instantiate(shipPrefab, transform.position, transform.rotation);
-		go.GetComponent<ShipAI>().Initialize(difficulty);
-	}
+    void Start()
+    {
+        difficulty = 0;
+        speed = 1;
+        score = 0;
+        isPlaying = true;
+        SpawnShip();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Spawn a single ship
+    void SpawnShip()
+    {
+        GameObject go = (GameObject)Instantiate(shipPrefab, Spawn1.position, transform.rotation);
+        go.GetComponent<ShipAI>().Initialize(difficulty, speed, this.gameObject.GetComponent<Transform>());
+    }
+
+    public void gameOver()
+    {
+        isPlaying = false;
+
+    }
 }
--- Assets/Scripts/ShipAI.cs
+++ Assets/Scripts/ShipAI.cs
@@ -2,42 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipAI : MonoBehaviour {
+public class ShipAI : MonoBehaviour
+{
 
-	bool solved;
-	int difficulty;
-	List<int> flagsRequired;
-	Transform destination;
+    bool solved;
+    int difficulty;
+    float speed;
+    List<int> flagsRequired;
+    Transform destination;
+    GameObject shipEater;
 
-	// Use this for initialization
-	void Start () {
-		flagsRequired = new List<int>();
-		solved = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	public void Initialize(int newDifficulty) {
-		difficulty = newDifficulty;
-	}
-	
-	public void setDifficulty(int newDifficulty) {
-		difficulty = newDifficulty;
-	}
-	
-	
-	void RemoveFlag() {
-		//Remove the head of the required flags
-		flagsRequired.RemoveAt(0);
-		
-		//If there's no flags left then you're solved
-		if(flagsRequired.Count == 0) {
-			solved = true;
-		}
-	}
-	
-	
+    // Use this for initialization
+    void Start()
+    {
+        flagsRequired = new List<int>();
+        solved = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, destination.position, step);
+        if (this.gameObject.GetComponent<Collider>().bounds.Intersects(shipEater.GetComponent<Collider>().bounds))
+        {
+            Destroy(this.gameObject);
+            GameObject.Find("DefaultGamemode").GetComponent<Gamemode>().gameOver();
+        }
+    }
+
+    public void Initialize(int newDifficulty, float newSpeed, Transform newDestination)
+    {
+        difficulty = newDifficulty;
+        speed = newSpeed;
+        destination = newDestination;
+        transform.LookAt(destination);
+        shipEater = GameObject.Find("ShipEater");
+    }
+
+    public void setDifficulty(int newDifficulty)
+    {
+        difficulty = newDifficulty;
+    }
+
+
+    void RemoveFlag()
+    {
+        //Remove the head of the required flags
+        flagsRequired.RemoveAt(0);
+
+        //If there's no flags left then you're solved
+        if (flagsRequired.Count == 0)
+        {
+            solved = true;
+        }
+    }
+
+
 }
